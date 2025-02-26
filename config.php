23a361c6a4254b96d461bc605192c7f9d6b91df5
ankurojha834 <ankurojha175@gmail.com>
2025-02-26 09:48:16 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,1 @@
-<?php
-define("WEATHER_API_KEY", "a9a329a686a72abe74a4f4116d0ddac1");
-define("AQI_API_KEY", "1a2c40c29985664dbce105f39aec60b87274ac92");
-define("GEMINI_API_KEY", "AIzaSyAq9w5jwPg735ArPZvAUun5R4edSp6Qb34");
 
-?>
